eea7ae2815cbdb5050cbde743fbc61f3127fdb47
root <root@se-hdp251.field.hortonworks.com>
2017-04-08 19:30:32 +0000

    updated install sh

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
 
 unzip OnlineRetail.txt.zip
+su - admin -c "hdfs dfs -mkdir -p /user/admin/retail/retailsalesraw"
+cp OnlineRetail.txt /tmp
+chmod 777 /tmp/OnlineRetail.txt
+su - admin -c "hdfs dfs -put /tmp/OnlineRetail.txt /user/admin/retail/retailsalesraw/"
+cp *.pig /tmp
+cp *.ddl /tmp
+chmod 777 /tmp/*.pig
+chmod 777 /tmp/*.ddl
+su - admin -c "pig /tmp/RetailSalesIngestion.pig"
+su - admin -c "pig /tmp/MBADataPrep.pig"
+su - admin -c "hive -f /tmp/RetailSalesRaw.ddl"
+su - admin -c "hive -f /tmp/RetailSales.ddl"
+
+# Import following notebook in Zeppelin 
+# URL = https://raw.githubusercontent.com/sainib/RetailAnalytics/master/RetailSalesAnalytics.json
 
